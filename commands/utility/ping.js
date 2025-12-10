1944f301c88ed20cd048542b3764ce84b1bf9a32
gyrropro <gabrielbor.1324@gmail.com>
2025-12-10 12:48:07 -0500

    ping command updated using embed

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,8 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!, this command worked!');
+		const exampleEmbed = new EmbedBuilder()
+	.setColor(0xDE8050)
+	.setTitle('Pong!')
+	.setDescription('you have pinged the bot sucsessfuly!')
+	.setTimestamp()
+	.setFooter({ text: '©2025 copper', iconURL: 'https://i.imgur.com/StODuzm.png' });
+
+		await interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
