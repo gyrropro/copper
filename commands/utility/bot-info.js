b8cd40137a0bf9baf6a126b3683e6a2d19a84581
gyrropro <gabrielbor.1324@gmail.com>
2025-12-11 10:17:42 -0500

    bot insfo command added

---
+++ commands/utility/bot-info.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, EmbedBuilder, InteractionContextType } = require('discord.js');
+const botInfo = require('../../package.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bot')
+        .setDescription('Bot-related commands')
+        .addSubcommand((subcommand) =>
+		subcommand
+			.setName('info')
+			.setDescription('Info about copper')
+	)
+
+        .setContexts(InteractionContextType.Guild, InteractionContextType.PrivateChannel, InteractionContextType.BotDM),
+
+    async execute(interaction) {
+
+        if(interaction.options.getSubcommand() === 'info') {
+                    const exampleEmbed = new EmbedBuilder()
+            .setColor(0xDE8050)
+            .setTitle('bot info:')
+            .setDescription(botInfo.description)
+            .addFields(
+                { name: 'author:', value: botInfo.author, inline: true },
+                { name: "servers:", value: interaction.client.guilds.cache.size.toLocaleString(), inline: true }
+            )
+            .addFields(
+                { name: 'commands:', value: interaction.client.commands.size.toString(), inline: true },
+                { name: "add me to your server!", value: 'https://discord.com/oauth2/authorize?client_id=1324168158612820038', inline: false },
+                { name: "join the discord server!", value: 'https://discord.gg/U9NDF4rJQh', inline: false }
+            )
+            .setTimestamp()
+            .setFooter({
+                text: '©2025 copper',
+                iconURL: 'https://i.imgur.com/StODuzm.png'
+            });
+
+            await interaction.reply({ embeds: [exampleEmbed]});
+        }
+    },
+};
